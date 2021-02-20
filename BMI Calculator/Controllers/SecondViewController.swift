e062f5fa5054eff578e88d98126ea07067e51391
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-02-20 11:59:07 -0300

    Criando SecondViewController para tela de calculo de IMC

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Jean Camargo on 20/02/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
