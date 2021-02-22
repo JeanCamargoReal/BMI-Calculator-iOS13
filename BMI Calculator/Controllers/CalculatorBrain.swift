1dafca064268118b1d01a42cb054643354a0782c
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-02-22 20:14:46 -0300

    Adicionando lógica dos pesos do IMC

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,18 +6,27 @@
 //  Copyright © 2021 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float = 0.0
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / (height * height)
+        let bmiValue = weight / (height * height)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Coma mais pizza!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "O shape está em dia", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "PARE de comer pizza, agora!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+        }
+        
     }
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0 )
         return bmiTo1DecimalPlace
     }
 }
